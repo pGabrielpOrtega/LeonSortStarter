bcf87abd6eba332d4ab9c5d68f8652366f8f525f
gabri <gabriel_arturo@yahoo.com>
2020-03-19 20:55:15 -0600

    Solucione algunos errores para poder tener el arreglo originaly una copia a así verificar si los arreglos funcionan correctamente

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -15,26 +15,48 @@ public class Main {
         BubleSort Bub = new BubleSort();
         MergeSort m = new MergeSort();
 
-        // aqui pregunto si quiere ser po un arreglo ya hehco o aletorio
-        int[] elementos = {3,2,4,5,6,7,9,9,10,1,3,5};
+        // aqui pongo el arreglo aletorio y copie el mismo para las comprobaciones en un nuevo arreglo
+        //int[] elementos = {3,2,4,5,6,7,9,9,10,1,3,5};
+        int z = 100;
+        int[] elementos = new int[z];
+        int[] e1 = new int[z];
+        Random r = new Random();
 
+        for (int i = 0; i < elementos.length; i++) {
+            elementos[i] = Math.abs(r.nextInt(1000));
+        }
+
+        System.arraycopy(elementos,0,e1,0,elementos.length-1);
         //Imprimo el arreglo como se ve
-        System.out.println((Arrays.toString(elementos)));
+            System.out.println((Arrays.toString(elementos)));
+        System.out.println((Arrays.toString(e1)));
         //Imrpimo el tiempo de cada algoritmo
-        //Merge
-        System.out.println("\n\ntarda "+ m.sort(elementos)+ " milisegundos");
-//heap
-        System.out.println("\n\ntarda "+ heap.sort(elementos)+ " milisegundos");
-//Bubble
-        System.out.println("\n\ntarda "+ Bub.sort(elementos)+ " milisegundos");
-        //Quick sort
-        System.out.println("\n\ntarda "+ qk.sort(elementos)+ " milisegundos");
+        // Merge
+            System.out.println("\n\nMerge Sort tarda "+ m.sort(elementos)+ " milisegundos");
 
+        //heap
+            System.out.println("\n\nHeap Sort tarda "+ heap.sort(elementos)+ " milisegundos");
+
+        //Bubble
+            System.out.println("\n\nBubble sortarda "+ Bub.sort(elementos)+ " milisegundos");
+
+        //Quick sort
+            System.out.println("\n\nQuick Sort tarda "+ qk.sort(elementos)+ " milisegundos");
+
+
+
+        int x = leerEntero(sc,"Ingrese El NUMERO .-1 si desea ver que los algoritmos funcionan correctamente, cualquier otro numero para NO ","Error No se reconocio respuesta");
+        if(x ==1){
+            System.out.println(Arrays.toString(e1));
+            System.arraycopy(e1,0,e1,0,elementos.length-1);
+            qk.quickSort( e1, 0, elementos.length - 1 );
+            System.out.println("Quick sort \n"+Arrays.toString(e1));
+        }
         // Let's sort using quick sort
-        qk.quickSort( elementos, 0, elementos.length - 1 );
+
 
         // Verify sorted array
-        System.out.println(Arrays.toString(elementos));
+
 
 
 
@@ -43,7 +65,18 @@ public class Main {
 
 
     }
-}
+
+    public static int leerEntero(Scanner sc,String mensaje, String mensajeError) {
+        int retval = 0;
+        System.out.println(mensaje);
+        while (!sc.hasNextInt()) {
+            sc.nextLine();
+            System.out.println(mensajeError);
+        }
+        retval = sc.nextInt();
+        sc.nextLine();
+        return retval;
+    }}
 // for (int i = 0; i < elementos.length; i++) {
 //   System.out.println(elementos[i]);
 // }
