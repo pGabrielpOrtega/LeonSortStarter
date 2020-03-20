33b18115c80633ee829d53372a566f6064574df8
gabri <gabriel_arturo@yahoo.com>
2020-03-19 19:24:18 -0600

    trabajando en el main

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -1,15 +1,26 @@
 package edu.ujcv.progra1;
-
+import java.util.Random;
 import java.util.Arrays;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        //Llamo a las demás clases
+        ShuffleSort s = new ShuffleSort();
+        HeapSort heap = new HeapSort();
+        QuickSort qk = new QuickSort();
+        BubleSort Bub = new BubleSort();
+
+        // aqui pregunto si quiere ser po un arreglo ya hehco o aletorio
+        System.out.println("Desea un arreglo desordenado del 1 al 10 sencillo para ver las funciona correctamente \no un arrelgo desordena aletorio");
+        int 
         int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
         Integer[] array = new Integer[] {3,2,4,5,6,7,9,9,10,3,5};
 
-        ShuffleSort s = new ShuffleSort();
+
         elementos = s.shuffleSort(elementos);
 
         for (int i = 0; i < elementos.length; i++) {
@@ -18,7 +29,7 @@ public class Main {
 
         elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
         System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
-        QuickSort qk = new QuickSort();
+
 
 
         // Let's sort using quick sort
