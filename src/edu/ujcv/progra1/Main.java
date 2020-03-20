7ffad2d757ca75745d06c74ecb3a458972c8fb08
gabri <gabriel_arturo@yahoo.com>
2020-03-19 21:52:07 -0600

    Puse en shuffle por si acaso, pero con un menu para no hacerse tan pesado

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Scanner sc = new Scanner(System.in);
-
+        int y = leerEntero(sc,"Desea usar el Shuffle ya que es muy lento \n Si.-1 \n No cualquier otra tecla siendo esta un numero ","Error No se reconocio respuesta xd");
         //Llamo a las demás clases
         ShuffleSort s = new ShuffleSort();
         HeapSort heap = new HeapSort();
@@ -16,14 +16,13 @@ public class Main {
         MergeSort m = new MergeSort();
 
         // aqui pongo el arreglo aletorio y copie el mismo para las comprobaciones en un nuevo arreglo
-
-        int z = 10;
+        int z = 10; // z es el numero del tamaño del arreglo
         int[] elementos = new int[z];
         int[] e1 = new int[z];
         Random r = new Random();
 
         for (int i = 0; i < elementos.length; i++) {
-            elementos[i] = Math.abs(r.nextInt(1000));
+            elementos[i] = Math.abs(r.nextInt(1000)); //este es numero de numeros
         }
 
         System.arraycopy(elementos,0,e1,0,elementos.length-1);
@@ -43,10 +42,13 @@ public class Main {
 
         //Quick sort
             System.out.println("\nQuick Sort tarda "+ qk.sort(elementos)+ " milisegundos");
+        //ShuffleSort
+        if(y==1){
+            System.out.println("\nShuffle Sort tarda "+ s.sort(elementos)+ " milisegundos");}
 
 
 
-        int x = leerEntero(sc,"Ingrese el numero .-1 si desea ver que los algoritmos funcionan correctamente, cualquier otro numero para NO ","Error No se reconocio respuesta xd");
+        int x = leerEntero(sc,"\nDesea ver si los algoritmos funcioan correctamente \n 1.-Si \n Cualquier otra tecla para no siendo está un numero","Error No se reconocio respuesta xd");
         if(x ==1){
             //aqui copío los arreglos
             int[] e2 = new int[z];
@@ -57,6 +59,9 @@ public class Main {
 
             int[] e4 = new int[z];
             System.arraycopy(e1,0,e4,0,elementos.length-1);
+
+            int[] e5 = new int[z];
+            System.arraycopy(e1,0,e5,0,elementos.length-1);
 
             System.out.println("El Arreglo origiinal \n"+Arrays.toString(e1));
 
@@ -72,7 +77,10 @@ public class Main {
             System.out.println("\n Heap Sort \n"+Arrays.toString(e3));
 
             Bub.bubleSort(e4);
-            System.out.println("\nBubble Sort \n"+Arrays.toString(e4)+"\n");
+            System.out.println("\nBubble Sort \n"+Arrays.toString(e4));
+if(y==1){
+            e5 = s.shuffleSort(e5);
+            System.out.println("\nShuffle Sort \n"+Arrays.toString(e5));}
         }
 
 
