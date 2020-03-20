820f72d52c8b454950a308007cc87031f74b22db
gabri <gabriel_arturo@yahoo.com>
2020-03-19 19:02:24 -0600

    Puse el merge sort

--- src/edu/ujcv/progra1/Main.java
+++ src/edu/ujcv/progra1/Main.java
@@ -7,6 +7,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         int[] elementos = {3,2,4,5,6,7,9,9,10,3,5};
+        Integer[] array = new Integer[] {3,2,4,5,6,7,9,9,10,3,5};
+
         ShuffleSort s = new ShuffleSort();
         elementos = s.shuffleSort(elementos);
 
@@ -17,12 +19,13 @@ public class Main {
         elementos = new int[]{3,2,4,5,6,7,9,9,10,3,5};
         System.out.println("\n\ntarda "+ s.sort(elementos)+ " milisegundos");
         QuickSort qk = new QuickSort();
-        Integer[] array = new Integer[] { 12, 13, 24, 10, 3, 6, 90, 70 };
+
 
         // Let's sort using quick sort
         qk.quickSort( array, 0, array.length - 1 );
 
         // Verify sorted array
         System.out.println(Arrays.toString(array));
+
     }
 }
--- src/edu/ujcv/progra1/MergeSort.java
+++ src/edu/ujcv/progra1/MergeSort.java
@@ -11,12 +11,37 @@ public class MergeSort  implements SortTester{
         return end - start;
     }
 
-    public static int[] mergeSort(int[] a){
-        return new int[5];
+    private static int[] merge(int[] a, int[] b) {
+        int[] c = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < a.length && j < b.length){
+            if (a[i] < b[j]) {
+                c[k++] = a[i++];
+            }else {
+                c[k++] = b[j++];
+            }
+        }
+        if(i < a.length){
+            System.arraycopy(a,i,c,k,c.length - k);
+        }else{
+            System.arraycopy(b,j,c,k,c.length - k);
+        }
+        return  c;
     }
+    public static int[] mergeSort(int[] arr){
+        if(arr.length <= 1){
+            return arr;
+        }
+        int[] inf = new int[arr.length/2];
+        int[] sup = new int[arr.length - inf.length];
 
-    public static int[] merge(int[] inf, int[] sup ){
-        return new int[5];
+        System.arraycopy(arr,0,inf,0,inf.length);
+        System.arraycopy(arr,inf.length,sup,0,sup.length);
+
+        return merge(mergeSort(inf),mergeSort(sup));
+
     }
 
 
